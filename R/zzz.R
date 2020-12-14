4ce745435df150e82864164245820408f11ad175
Rory Stark <rory.stark@cruk.cam.ac.uk>
2020-12-14 15:54:44 +0000

    add onAttach and Rmd typos

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage(">>>>>> Quantitative analysis of ChiP-seq, ATAC-seq, and related DNA enrichment assays\n")
+  packageStartupMessage("You can follow the complete workshop in the Help tab as follows:\n")
+  packageStartupMessage("   vignette(\"Quantitative-ChIPseq-Workshop\",\"QuantitativeChIPseqWorkshop\")")
+}
+
